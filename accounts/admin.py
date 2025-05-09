710d1661ca4ddcaf87ab5edaa4a71847b7940bd5
John Endahl <13946317+JEndahl53@users.noreply.github.com>
2025-05-09 12:07:50 -0400

    added custom user model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,26 @@
+# accounts/admin.py
+#
 from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
+
+from .forms import CustomUserCreationForm, CustomUserChangeForm
+from .models import CustomUser
+
 
 # Register your models here.
+class CustomUserAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = CustomUser
+    list_display = [
+        'email',
+        'username',
+        'first_name',
+        'last_name',
+        'is_staff',
+        'is_active',
+        'is_superuser',
+    ]
+
+admin.site.register(CustomUser, CustomUserAdmin)
+
--- core/settings.py
+++ core/settings.py
@@ -131,3 +131,4 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+AUTH_USER_MODEL = "accounts.CustomUser"
